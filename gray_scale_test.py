fd5666afbc6cdd9ecf1948c9ddb0532ab122f2d8
jostan86 <jostanbrown86@gmail.com>
2023-01-24 12:33:45 -0800

    update

---
+++ gray_scale_test.py
@@ -0,0 +1,16 @@
+from picarx_improved import Picarx
+import time
+
+
+if __name__ == "__main__":
+    px = Picarx()
+    x = 0
+    while x > 15:
+        x+=1
+        print(px.grayscale.get_grayscale_data())
+        time.sleep(1)
+
+
+
+
+
