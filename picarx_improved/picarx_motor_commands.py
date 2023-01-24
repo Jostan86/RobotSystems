0e523ac4916e7418b4addc9f804b22cc2a2e46ee
Jostan86 <jostanbrown86@gmail.com>
2023-01-23 22:29:49 -0800

    added motor commands file, not implemented yet

--- picarx_improved/picarx_motor_commands.py
+++ picarx_improved/picarx_motor_commands.py
@@ -1,10 +1,9 @@
-
 import logging
 # from logdecorator import log_on_start , log_on_end , log_on_error
 import os
+import math
 import time
 import atexit
-import math
 try :
     from robot_hat import *
     from robot_hat import reset_mcu
@@ -22,9 +21,12 @@ logging.getLogger().setLevel(logging.DEBUG)
 # user and User home directory
 User = os.popen('echo ${SUDO_USER:-$LOGNAME}').readline().strip()
 UserHome = os.popen('getent passwd %s | cut -d: -f 6'%User).readline().strip()
+# print(User)  # pi
+# print(UserHome) # /home/pi
 config_file = '%s/.config/picar-x/picar-x.conf'%UserHome
 
-class Picarx(object):
+
+class motor_commands(object):
     PERIOD = 4095
     PRESCALER = 10
     TIMEOUT = 0.02
@@ -35,25 +37,19 @@ class Picarx(object):
     # ultrasonic_pins: tring, echo
     # config: path of config file
     def __init__(self, 
-                servo_pins:list=['P0', 'P1', 'P2'], 
+                servo_pins:list=['P2'],
                 motor_pins:list=['D4', 'D5', 'P12', 'P13'],
-                grayscale_pins:list=['A0', 'A1', 'A2'],
-                ultrasonic_pins:list=['D2','D3'],
                 config:str=config_file,
                 ):
 
         # config_flie
         self.config_flie = fileDB(config, 774, User)
-        # servos init 
-        self.camera_servo_pin1 = Servo(PWM(servo_pins[0]))
-        self.camera_servo_pin2 = Servo(PWM(servo_pins[1]))   
-        self.dir_servo_pin = Servo(PWM(servo_pins[2])) 
+        # servos init
+        self.dir_servo_pin = Servo(PWM(servo_pins[0]))
         self.dir_cal_value = int(self.config_flie.get("picarx_dir_servo", default_value=0))
-        self.cam_cal_value_1 = int(self.config_flie.get("picarx_cam_servo1", default_value=0))
-        self.cam_cal_value_2 = int(self.config_flie.get("picarx_cam_servo2", default_value=0))
+
         self.dir_servo_pin.angle(self.dir_cal_value)
-        self.camera_servo_pin1.angle(self.cam_cal_value_1)
-        self.camera_servo_pin2.angle(self.cam_cal_value_2)
+
         # motors init
         self.left_rear_dir_pin = Pin(motor_pins[0])
         self.right_rear_dir_pin = Pin(motor_pins[1])
@@ -68,15 +64,7 @@ class Picarx(object):
         for pin in self.motor_speed_pins:
             pin.period(self.PERIOD)
             pin.prescaler(self.PRESCALER)
-        # grayscale module init
-        # usage: self.grayscale.get_grayscale_data()
-        adc0, adc1, adc2 = grayscale_pins
-        self.grayscale = Grayscale_Module(adc0, adc1, adc2, reference=1000)
-        # ultrasonic init
-        # usage: distance = self.ultrasonic.read()
-        tring, echo= ultrasonic_pins
-        self.ultrasonic = Ultrasonic(Pin(tring), Pin(echo))
-        
+
 
     def set_motor_speed(self,motor,speed):
         # global cali_speed_value,cali_dir_value
@@ -128,23 +116,6 @@ class Picarx(object):
         self.dir_current_angle = value
         angle_value  = value + self.dir_cal_value
         self.dir_servo_pin.angle(angle_value)
-
-    def camera_servo1_angle_calibration(self,value):
-        self.cam_cal_value_1 = value
-        self.config_flie.set("picarx_cam_servo1", "%s"%value)
-        self.camera_servo_pin1.angle(value)
-
-    def camera_servo2_angle_calibration(self,value):
-        self.cam_cal_value_2 = value
-        self.config_flie.set("picarx_cam_servo2", "%s"%value)
-        self.camera_servo_pin2.angle(value)
-
-    def set_camera_servo1_angle(self,value):
-        self.camera_servo_pin1.angle(-1*(value + -1*self.cam_cal_value_1))
-
-    def set_camera_servo2_angle(self,value):
-        self.camera_servo_pin2.angle(-1*(value + -1*self.cam_cal_value_2))
-
 
     def set_power(self,speed):
         self.set_motor_speed(1, speed)
