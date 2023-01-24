a58f72975f479db1121e3cf875f2c7378249f27f
jostan86 <jostanbrown86@gmail.com>
2023-01-24 14:43:43 -0800

    update

--- line_follow.py
+++ line_follow.py
@@ -11,55 +11,75 @@
 from picarx import Picarx
 from time import sleep
 
-px = Picarx()
+
 # px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
 # px.set_grayscale_reference(200)
 # px.grayscale.reference = 1400
-px.set_grayscale_reference(150)
-last_state = None
-current_state = None
-px_power = 10
-offset = 20
+# px.set_grayscale_reference(200)
+# last_state = None
+# current_state = None
+# px_power = 10
+# offset = 20
 
 
-def outHandle():
-    global last_state, current_state
-    if last_state == 'left':
-        px.set_dir_servo_angle(-30)
-        px.backward(10)
-    elif last_state == 'right':
-        px.set_dir_servo_angle(30)
-        px.backward(10)
-    while True:
-        gm_val_list = px.get_grayscale_data()
-        gm_state = px.get_line_status(gm_val_list)
-        print("outHandle gm_val_list: %s, %s"%(gm_val_list, gm_state))
-        currentSta = gm_state
-        if currentSta != last_state:
-            break
-    sleep(0.001)
+# def outHandle():
+#     global last_state, current_state
+#     if last_state == 'left':
+#         px.set_dir_servo_angle(-30)
+#         px.backward(10)
+#     elif last_state == 'right':
+#         px.set_dir_servo_angle(30)
+#         px.backward(10)
+#     while True:
+#         gm_val_list = px.get_grayscale_data()
+#         gm_state = px.get_line_status(gm_val_list)
+#         print("outHandle gm_val_list: %s, %s"%(gm_val_list, gm_state))
+#         currentSta = gm_state
+#         if currentSta != last_state:
+#             break
+#     sleep(0.001)
 
 
 if __name__=='__main__':
-    try:
-        while True:
-            gm_val_list = px.get_grayscale_data()
-            gm_state = px.get_line_status(gm_val_list)
-            print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
+    px = Picarx()
+    while True:
+        gm_val_list = px.get_grayscale_data()
+        gm_state = px.get_line_status(gm_val_list)
+        print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
+        if gm_state == "stop":
+            px.stop()
+        elif gm_state == "right":
+            px.set_dir_servo_angle(10)
+            px.forward(30)
+        elif gm_state == "left":
+            px.set_dir_servo_angle(-10)
+            px.forward(30)
+        elif gm_state == "forward":
+            px.forward(30)
+        else:
+            px.stop()
+        sleep(.01)
 
-            if gm_state != "stop":
-                last_state = gm_state
 
-            if gm_state == 'forward':
-                px.set_dir_servo_angle(0)
-                px.forward(px_power)
-            elif gm_state == 'left':
-                px.set_dir_servo_angle(offset)
-                px.forward(px_power)
-            elif gm_state == 'right':
-                px.set_dir_servo_angle(-offset)
-                px.forward(px_power)
-            else:
-                outHandle()
-    finally:
-        px.stop()
+    # try:
+    #     while True:
+    #         gm_val_list = px.get_grayscale_data()
+    #         gm_state = px.get_line_status(gm_val_list)
+    #         print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
+    #
+    #         if gm_state != "stop":
+    #             last_state = gm_state
+    #
+    #         if gm_state == 'forward':
+    #             px.set_dir_servo_angle(0)
+    #             px.forward(px_power)
+    #         elif gm_state == 'left':
+    #             px.set_dir_servo_angle(offset)
+    #             px.forward(px_power)
+    #         elif gm_state == 'right':
+    #             px.set_dir_servo_angle(-offset)
+    #             px.forward(px_power)
+    #         else:
+    #             outHandle()
+    # finally:
+    #     px.stop()
