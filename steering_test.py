5a9a8f7ef7c68ae7e16d796319ccd84943f6f2ac
Jostan86 <jostanbrown86@gmail.com>
2023-01-23 21:51:36 -0800

    update

--- steering_test.py
+++ steering_test.py
@@ -7,14 +7,17 @@ if __name__ == "__main__":
     px = Picarx()
     px.forward(40)
     time.sleep(2)
+    px.stop()
     px.set_dir_servo_angle(40)
     time.sleep(0.5)
     px.backward(40)
     time.sleep(1.5)
+    px.stop()
     px.set_dir_servo_angle(-40)
     time.sleep(0.5)
     px.backward(40)
     time.sleep(1.5)
+    px.stop()
 
 
 
