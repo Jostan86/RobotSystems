90c91c5c42508232eca39cd8e304b9b2759b001f
jostan86 <jostanbrown86@gmail.com>
2023-01-24 14:15:22 -0800

    update

--- picarx_improved/picarx_improved.py
+++ picarx_improved/picarx_improved.py
@@ -14,8 +14,8 @@ except ImportError :
            "calls with substitute functions ")
     from sim_robot_hat import *
 
-from picax_motor_commands import MotorCommands
-from sim_robot_hat import Grayscale_Module
+# from picax_motor_commands import MotorCommands
+# from sim_robot_hat import Grayscale_Module
 
 logging_format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
@@ -27,13 +27,18 @@ UserHome = os.popen('getent passwd %s | cut -d: -f 6'%User).readline().strip()
 config_file = '%s/.config/picar-x/picar-x.conf'%UserHome
 
 class Picarx(object):
+    PERIOD = 4095
+    PRESCALER = 10
+    TIMEOUT = 0.02
 
-    # servo_pins: camera_servo_1, camera_servo_2
+    # servo_pins: direction_servo, camera_servo_1, camera_servo_2
+    # motor_pins: left_swicth, right_swicth, left_pwm, right_pwm
     # grayscale_pins: 3 adc channels
     # ultrasonic_pins: tring, echo
     # config: path of config file
-    def __init__(self, 
-                servo_pins:list=['P0', 'P1'],
+    def __init__(self,
+                servo_pins:list=['P0', 'P1', 'P2'],
+                motor_pins:list=['D4', 'D5', 'P12', 'P13'],
                 grayscale_pins:list=['A0', 'A1', 'A2'],
                 ultrasonic_pins:list=['D2','D3'],
                 config:str=config_file,
@@ -41,25 +46,199 @@ class Picarx(object):
 
         # config_flie
         self.config_flie = fileDB(config, 774, User)
-        # servos init 
+        # servos init
         self.camera_servo_pin1 = Servo(PWM(servo_pins[0]))
-        self.camera_servo_pin2 = Servo(PWM(servo_pins[1]))   
+        self.camera_servo_pin2 = Servo(PWM(servo_pins[1]))
+        self.dir_servo_pin = Servo(PWM(servo_pins[2]))
+        self.dir_cal_value = int(self.config_flie.get("picarx_dir_servo", default_value=0))
         self.cam_cal_value_1 = int(self.config_flie.get("picarx_cam_servo1", default_value=0))
         self.cam_cal_value_2 = int(self.config_flie.get("picarx_cam_servo2", default_value=0))
+        self.dir_servo_pin.angle(self.dir_cal_value)
         self.camera_servo_pin1.angle(self.cam_cal_value_1)
         self.camera_servo_pin2.angle(self.cam_cal_value_2)
-
+        # motors init
+        self.left_rear_dir_pin = Pin(motor_pins[0])
+        self.right_rear_dir_pin = Pin(motor_pins[1])
+        self.left_rear_pwm_pin = PWM(motor_pins[2])
+        self.right_rear_pwm_pin = PWM(motor_pins[3])
+        self.motor_direction_pins = [self.left_rear_dir_pin, self.right_rear_dir_pin]
+        self.motor_speed_pins = [self.left_rear_pwm_pin, self.right_rear_pwm_pin]
+        self.cali_dir_value = self.config_flie.get("picarx_dir_motor", default_value="[1,1]")
+        self.cali_dir_value = [int(i.strip()) for i in self.cali_dir_value.strip("[]").split(",")]
+        self.cali_speed_value = [0, 0]
+        self.dir_current_angle = 0
+        for pin in self.motor_speed_pins:
+            pin.period(self.PERIOD)
+            pin.prescaler(self.PRESCALER)
         # grayscale module init
         # usage: self.grayscale.get_grayscale_data()
         adc0, adc1, adc2 = grayscale_pins
-        self.grayscale = Grayscale_Module(adc0, adc1, adc2, reference=780)
+        self.grayscale = Grayscale_Module(adc0, adc1, adc2, reference=1000)
         # ultrasonic init
         # usage: distance = self.ultrasonic.read()
         tring, echo= ultrasonic_pins
         self.ultrasonic = Ultrasonic(Pin(tring), Pin(echo))
 
-        self.motor_commands = MotorCommands()
-        
+    def set_motor_speed(self,motor,speed):
+        # global cali_speed_value,cali_dir_value
+        motor -= 1
+        if speed >= 0:
+            direction = 1 * self.cali_dir_value[motor]
+        elif speed < 0:
+            direction = -1 * self.cali_dir_value[motor]
+        speed = abs(speed)
+        # if speed != 0:
+            # speed = int(speed /2 ) + 50
+        speed = speed - self.cali_speed_value[motor]
+        if direction < 0:
+            self.motor_direction_pins[motor].high()
+            self.motor_speed_pins[motor].pulse_width_percent(speed)
+        else:
+            self.motor_direction_pins[motor].low()
+            self.motor_speed_pins[motor].pulse_width_percent(speed)
+
+    def motor_speed_calibration(self,value):
+        # global cali_speed_value,cali_dir_value
+        self.cali_speed_value = value
+        if value < 0:
+            self.cali_speed_value[0] = 0
+            self.cali_speed_value[1] = abs(self.cali_speed_value)
+        else:
+            self.cali_speed_value[0] = abs(self.cali_speed_value)
+            self.cali_speed_value[1] = 0
+
+    def motor_direction_calibration(self,motor, value):
+        # 1: positive direction
+        # -1:negative direction
+        motor -= 1
+        # if value == 1:
+        #     self.cali_dir_value[motor] = -1 * self.cali_dir_value[motor]
+        # self.config_flie.set("picarx_dir_motor", self.cali_dir_value)
+        if value == 1:
+            self.cali_dir_value[motor] = 1
+        elif value == -1:
+            self.cali_dir_value[motor] = -1
+        self.config_flie.set("picarx_dir_motor", self.cali_dir_value)
+
+    def dir_servo_angle_calibration(self,value):
+        self.dir_cal_value = value
+        self.config_flie.set("picarx_dir_servo", "%s"%value)
+        self.dir_servo_pin.angle(value)
+
+    def set_dir_servo_angle(self,value):
+        self.dir_current_angle = value
+        angle_value  = value + self.dir_cal_value
+        self.dir_servo_pin.angle(angle_value)
+
+    def set_power(self,speed):
+        self.set_motor_speed(1, speed)
+        self.set_motor_speed(2, speed)
+
+    def backward(self,speed):
+        axel_length = 117
+        axel_seperation = 95
+
+        current_angle = self.dir_current_angle
+        if current_angle != 0:
+            abs_current_angle = abs(current_angle)
+            # if abs_current_angle >= 0:
+            if abs_current_angle > 40:
+                abs_current_angle = 40
+            # power_scale = (100 - abs_current_angle) / 100.0
+            power_scale = (axel_length / math.tan(math.radians(current_angle)) - axel_seperation / 2) / (
+                        axel_length / math.tan(math.radians(current_angle)) + axel_seperation / 2)
+            if (current_angle / abs_current_angle) > 0:
+                self.set_motor_speed(1, -1*speed*power_scale)
+                self.set_motor_speed(2, speed)
+            else:
+                self.set_motor_speed(1, -1*speed)
+                self.set_motor_speed(2, speed / power_scale)
+        else:
+            self.set_motor_speed(1, -1*speed)
+            self.set_motor_speed(2, speed)
+
+    def forward(self,speed):
+        current_angle = self.dir_current_angle
+        axel_length = 117
+        axel_seperation = 95
+        if current_angle != 0:
+            abs_current_angle = abs(current_angle)
+            # if abs_current_angle >= 0:
+            if abs_current_angle > 40:
+                abs_current_angle = 40
+
+            power_scale = (axel_length/math.tan(math.radians(current_angle)) - axel_seperation/2) / (axel_length/math.tan(math.radians(current_angle)) + axel_seperation/2)
+
+            if (current_angle / abs_current_angle) > 0:
+
+                self.set_motor_speed(1, speed * power_scale)
+                self.set_motor_speed(2, -speed)
+            else:
+                self.set_motor_speed(1, speed)
+                self.set_motor_speed(2, -speed / power_scale)
+        else:
+            self.set_motor_speed(1, speed)
+            self.set_motor_speed(2, -1*speed)
+
+    def stop(self):
+        self.set_motor_speed(1, 0)
+        self.set_motor_speed(2, 0)
+
+    def forward_distance(self, distance):
+        ...
+
+    def backward_distance(self, distance):
+        ...
+
+    def parallel_park(self, side="right"):
+        self.set_dir_servo_angle(0)
+        time.sleep(0.5)
+        self.forward(40)
+        time.sleep(1.3)
+        self.stop()
+        if side == "right":
+            self.set_dir_servo_angle(40)
+        else:
+            self.set_dir_servo_angle(-40)
+        time.sleep(0.5)
+        self.backward(40)
+        time.sleep(1.5)
+        self.stop()
+        if side == "right":
+            self.set_dir_servo_angle(-40)
+        else:
+            self.set_dir_servo_angle(40)
+        time.sleep(0.5)
+        self.backward(40)
+        time.sleep(1.3)
+        self.stop()
+        self.set_dir_servo_angle(0)
+        time.sleep(0.5)
+        self.stop()
+
+    def k_turn(self, dir="left"):
+
+        if dir=="left":
+            self.set_dir_servo_angle(-30)
+        else:
+            self.set_dir_servo_angle(30)
+        time.sleep(0.5)
+        self.forward(50)
+        time.sleep(1.6)
+        self.stop()
+        if dir=="left":
+            self.set_dir_servo_angle(30)
+        else:
+            self.set_dir_servo_angle(-30)
+        time.sleep(0.5)
+        self.backward(50)
+        time.sleep(1.6)
+        self.stop()
+        self.set_dir_servo_angle(0)
+        time.sleep(0.5)
+        self.forward(40)
+        time.sleep(.8)
+        self.stop()
     def camera_servo1_angle_calibration(self,value):
         self.cam_cal_value_1 = value
         self.config_flie.set("picarx_cam_servo1", "%s"%value)
@@ -94,7 +273,33 @@ class Picarx(object):
     def backward_distance(self, distance):
         ...
 
+class Grayscale_Module(object):
+    def __init__(self, pin0, pin1, pin2, reference=1000):
+        self.chn_0 = ADC(pin0)
+        self.chn_1 = ADC(pin1)
+        self.chn_2 = ADC(pin2)
+        self.reference = reference
 
+    def get_line_status(self, fl_list):
+
+        if fl_list[0] > self.reference and fl_list[1] > self.reference and fl_list[2] > self.reference:
+            return 'stop1'
+
+        elif fl_list[1] <= self.reference:
+            return 'forward1'
+
+        elif fl_list[0] <= self.reference:
+            return 'right1'
+
+        elif fl_list[2] <= self.reference:
+            return 'left1'
+
+    def get_grayscale_data(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn_0.read())
+        adc_value_list.append(self.chn_1.read())
+        adc_value_list.append(self.chn_2.read())
+        return adc_value_list
 
 if __name__ == "__main__":
     px = Picarx()
