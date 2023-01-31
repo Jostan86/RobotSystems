8cb7503e4d2c5be9fbc44913c182faf66be44333
Jostan86 <jostanbrown86@gmail.com>
2023-01-30 21:35:01 -0800

    update line follow code

--- example/color_detect.py
+++ example/color_detect.py
@@ -94,12 +94,15 @@ with PiCamera() as camera:
     rawCapture = PiRGBArray(camera, size=camera.resolution)  
     time.sleep(2)
 
-    for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):# use_video_port=True
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         img = frame.array
         img,img_2,img_3 =  color_detect(img,'red')  # Color detection function
-        cv2.imshow("video", img)    # OpenCV image show
-        cv2.imshow("mask", img_2)    # OpenCV image show
-        cv2.imshow("morphologyEx_img", img_3)    # OpenCV image show
+        try:
+            cv2.imshow("video", img)    # OpenCV image show
+            cv2.imshow("mask", img_2)    # OpenCV image show
+            cv2.imshow("morphologyEx_img", img_3)    # OpenCV image show
+        except cv2.error:
+            print("no screen to show on")
         rawCapture.truncate(0)   # Release cache
     
         k = cv2.waitKey(1) & 0xFF
