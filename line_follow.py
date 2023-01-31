59212efb871ff342ffac7f8fda5445a8eeff3160
Jostan86 <jostanbrown86@gmail.com>
2023-01-30 16:39:47 -0800

    update

--- line_follow.py
+++ line_follow.py
@@ -12,8 +12,8 @@ from picarx_improved import Picarx
 from time import sleep
 
 def mapping_func(sensor_reading):
-    range_max = 300
-    range_min = 100
+    range_max = 370
+    range_min = 120
     range = range_max - range_min
     diff_left = (sensor_reading[0] - sensor_reading[1])/range
     diff_right = -(sensor_reading[2] - sensor_reading[1])/range
@@ -29,6 +29,7 @@ def mapping_func(sensor_reading):
 
 if __name__=='__main__':
     px = Picarx()
+    px.set_grayscale_reference(200)
     try:
 
         while True:
