bd5cc01997a471a5b369fa7c282bf497601d5a60
Jostan86 <jostanbrown86@gmail.com>
2023-01-31 22:08:12 -0800

    update line follow code

--- line_follow2.py
+++ line_follow2.py
@@ -280,18 +280,19 @@ if __name__=='__main__':
         # Detect lines using the Hough lines algorithm
         lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
 
-        # Draw the lines on the image
-        for line in lines:
-            rho, theta = line[0]
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
-            cv2.line(frame.array, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        if lines is not None:
+            # Draw the lines on the image
+            for line in lines:
+                rho, theta = line[0]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                cv2.line(frame.array, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
         # Show the image
         cv2.imshow("Line Detection", frame.array)
