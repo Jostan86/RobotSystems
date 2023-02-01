8e09ca5e5834f062c0eb567369dec5521763ce98
Jostan86 <jostanbrown86@gmail.com>
2023-02-01 06:43:23 -0800

    update line follow code

--- line_follow2.py
+++ line_follow2.py
@@ -294,78 +294,46 @@ if __name__=='__main__':
         #
         # # Apply thresholding to make the line black and the background white
         _, binary = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY_INV)
-        #
-        # # Find the edges of the line using the Canny edge detection algorithm
-        # edges = cv2.Canny(binary, 50, 150)
-        # #
-        # # Detect lines using the Hough lines algorithm
-        # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, np.empty(1), minLineLength=30, maxLineGap=10)
-        #
-        # if lines is not None:
-        # #  Draw the lines on the image
-        #    for line in lines:
-        #        x1, y1, x2, y2 = line[0]
-        #        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+
+        num_segments = 6
+
         # Get the height and width of the image
-        # height, width = img.shape[:2]
+        height, width = img.shape[:2]
+
+        segment_height = height // num_segments
+
+        for i in range(num_segments):
+            segment = binary[i * segment_height: (i + 1) * segment_height, :]
+            midpoint = find_2d_midpoint(segment)
+            if midpoint is not None:
+                midpoint[1] += int(i * (1/num_segments) * height)
+
+            cv2.circle(img, midpoint, 5, (255, 0, 0), -1)
+            # cv2.imshow(f"Segment {i + 1}", segment)
+
+
+        # segment1 = binary[0:segment_height, :]
+        # segment2 = binary[segment_height:2 * segment_height, :]
+        # segment3 = binary[2 * segment_height:3 * segment_height, :]
+        # segment4 = binary[3 * segment_height:, :]
         #
-        # # Split the image into 4 equal parts
-        # segment_height = height // 3
-        # for i in range(3):
-        #     segment = img[i * segment_height: (i + 1) * segment_height, :]
-        #     # Do something with the segment, such as processing or displaying
-        #     gray = cv2.cvtColor(segment, cv2.COLOR_BGR2GRAY)
         #
-        #     # Apply thresholding to make the line black and the background white
-        #     _, binary = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY_INV)
-        #
-        #     # Find the edges of the line using the Canny edge detection algorithm
-        #     edges = cv2.Canny(binary, 50, 150)
-        #     #
-        #     # Detect lines using the Hough lines algorithm
-        #     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, np.empty(1), minLineLength=30, maxLineGap=10)
-        #
-        #     if lines is not None:
-        #     #  Draw the lines on the image
-        #        for line in lines:
-        #            x1, y1, x2, y2 = line[0]
-        #            cv2.line(segment, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        #     # Get the height and width of the image
-        #     cv2.imshow(f"Segment {i + 1}", segment)
-        # Split the image into four segments
-        segment_height = img.shape[0] // 4
-        segment1 = binary[0:segment_height, :]
-        segment2 = binary[segment_height:2 * segment_height, :]
-        segment3 = binary[2 * segment_height:3 * segment_height, :]
-        segment4 = binary[3 * segment_height:, :]
-
-
-        # Find the midpoint of the black line in each segment
-        # def find_midpoint(segment):
-        #     nonzero_cols = np.nonzero(np.sum(segment, axis=0))[0]
-        #     if len(nonzero_cols) == 0:
-        #         return None
-        #     x_coords = np.arange(segment.shape[1])[nonzero_cols]
-        #     weights = np.sum(segment[:, nonzero_cols], axis=0)
-        #     return int(np.average(x_coords, weights=weights, axis=1))
-
-
-
-        midpoint1 = find_2d_midpoint(segment1)
-        midpoint2 = find_2d_midpoint(segment2)
-        midpoint3 = find_2d_midpoint(segment3)
-        midpoint4 = find_2d_midpoint(segment4)
-        height = img.shape[0]
-        if midpoint2 is not None:
-            midpoint2[1] += int(0.25 * height)
-        if midpoint3 is not None:
-            midpoint3[1] += int(0.5 * height)
-        if midpoint4 is not None:
-            midpoint4[1] += int(0.75 * height)
-        cv2.circle(img, midpoint1, 5, (255, 0, 0), -1)
-        cv2.circle(img, midpoint2, 5, (255, 0, 0), -1)
-        cv2.circle(img, midpoint3, 5, (255, 0, 0), -1)
-        cv2.circle(img, midpoint4, 5, (255, 0, 0), -1)
+        # midpoint1 = find_2d_midpoint(segment1)
+        # midpoint2 = find_2d_midpoint(segment2)
+        # midpoint3 = find_2d_midpoint(segment3)
+        # midpoint4 = find_2d_midpoint(segment4)
+        # height = img.shape[0]
+        # if midpoint2 is not None:
+        #     midpoint2[1] += int(0.25 * height)
+        # if midpoint3 is not None:
+        #     midpoint3[1] += int(0.5 * height)
+        # if midpoint4 is not None:
+        #     midpoint4[1] += int(0.75 * height)
+        # cv2.circle(img, midpoint1, 5, (255, 0, 0), -1)
+        # cv2.circle(img, midpoint2, 5, (255, 0, 0), -1)
+        # cv2.circle(img, midpoint3, 5, (255, 0, 0), -1)
+        # cv2.circle(img, midpoint4, 5, (255, 0, 0), -1)
 
 
 
