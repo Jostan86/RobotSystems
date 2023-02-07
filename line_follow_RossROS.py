a0e25f1d48043343954e24215f9c51b1beb51d92
jostan86 <jostanbrown86@gmail.com>
2023-02-07 13:51:07 -0800

    update line follow code

--- line_follow_RossROS.py
+++ line_follow_RossROS.py
@@ -67,31 +67,47 @@ class GS_sensor:
     def read_sensor(self):
         return px.get_grayscale_data()
 
+class US_sensor:
+    def __init__(self, px):
+        self.px = px
+
+    def read_US_sensor(self):
+        return px.get_distance()
+
+class US_controller:
+    def __init__(self, stop_treshold=8):
+        self.stop_threshold = stop_treshold
+
+    def US_controller(self, distance):
+        if distance > self.stop_threshold:
+            px.stop()
+
+
 
 
 
 if __name__=='__main__':
     px = Picarx()
     interpreter = GS_Line_Follow_Interpereter()
-    sensor = GS_sensor(px)
-    controller = Line_Follow_Controller(px)
-    sensor_bus_ros = rossros.Bus([0,0,0], 'GS_sensor_bus')
-    interpreter_bus_ros = rossros.Bus(0, 'GS_interpreter_bus')
+    gs_sensor = GS_sensor(px)
+    gs_controller = Line_Follow_Controller(px)
+    gs_sensor_bus = rossros.Bus([0,0,0], 'GS_sensor_bus')
+    gs_interpreter_bus = rossros.Bus(0, 'GS_interpreter_bus')
+    us_sensor = US_sensor(px)
+    us_controller = US_controller(px)
+    us_sensor_bus = rossros.Bus(0.0, 'US_sensor_bus')
+
     # Delay
     sensor_delay = 0.05
     interpreter_delay = 0.05
     controller_delay = .05
 
-    GS_sensor = rossros.Producer(sensor.read_sensor, sensor_bus_ros, delay=sensor_delay)
-    GS_interpreter = rossros.ConsumerProducer(interpreter.producer_consumer, sensor_bus_ros, interpreter_bus_ros, delay=interpreter_delay)
-    GS_controller = rossros.Consumer(controller.consumer, interpreter_bus_ros, delay=controller_delay)
+    GS_sensor = rossros.Producer(gs_sensor.read_sensor, gs_sensor_bus, delay=sensor_delay)
+    GS_interpreter = rossros.ConsumerProducer(interpreter.producer_consumer, gs_sensor_bus, gs_interpreter_bus, delay=interpreter_delay)
+    GS_controller = rossros.Consumer(gs_controller.consumer, gs_interpreter_bus, delay=controller_delay)
     rossros.runConcurrently([GS_sensor, GS_interpreter, GS_controller])
 
     # with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
     #     eSensor = executor.submit(sensor.producer_sensor, sensor_bus, sensor_delay)
     #     eInterpreter = executor.submit(interpreter.producer_consumer, sensor_bus, interpreter_bus, interpreter_delay)
     #     eController = executor.submit(controller.consumer, interpreter_bus, controller_delay)
-
-
-
-
